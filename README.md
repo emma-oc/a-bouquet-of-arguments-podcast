356d92a1da312f91ed1dca51605c3519a345623c
yue-duan <duanyue16@gmail.com>
2020-08-21 02:06:41 -0500

    update README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# a-bouquet-of-arguments-podcast
+# Welcome to the repo for our Podcast [`杠上开花 A Bouquet of Arguments`](https://podcasts.apple.com/us/podcast/%E6%9D%A0%E4%B8%8A%E5%BC%80%E8%8A%B1-a-bouquet-of-arguments/id1523066220)
+
+![logo](./pic/logo.jpg =250x250)
+
+很高兴遇见你！
+
+这里是「杠上开花」，一个严肃活泼的大型留学职场脑洞花式抬杠节目。
+一个严肃又活泼的大型留学职场脑洞花式抬杠节目。
+
+旨在「记录」和「分享」在美国留学到步入职场几年来的心得，留学职场，组队升级。
+
+我们的口号是：杠上开花，节节高。
+
+
+请关注我们的微信公众号与我们互动：
+
+![wechat](./pic/wechat.png =250x)
+
+我们的微博 [@杠上开花ABouquetOfArguments](https://www.weibo.com/u/7478127455)
+
+你可以在喜马拉雅上订阅我们：[https://www.ximalaya.com/gerenchengzhang/39801152/](https://www.ximalaya.com/gerenchengzhang/39801152/)
+
+也可以在Apple Podcast、Google Podcast、Spotify、Pocketcast等客户端收听我们的节目
+
+RSS feed：[https://anchor.fm/s/2aa384a8/podcast/rss](https://anchor.fm/s/2aa384a8/podcast/rss)
+
+
+Music attribution:
+Cheery Monday by Kevin MacLeod
+Link: incompetech.filmmusic.io/song/3495-cheery-monday
+License: creativecommons.org/licenses/by/4.0/
---
+++ ximalaya_data_crawling.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+import json
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+
+def main():
+
+    # define global variables
+    album_url = 'https://www.ximalaya.com/gerenchengzhang/39801152/'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+
+    output_path = '/Users/yue/Desktop/杠上开花/ximalaya/'
+
+
+    # get html file and parse
+
+    web_data = requests.get(album_url, 
+                            headers = headers).text
+    soup = BeautifulSoup(web_data)
+
+    main_script = soup.find('script', text=re.compile('window\.__INITIAL_STATE__'))
+    json_text = re.search(r'^\s*window\.__INITIAL_STATE__\s*=\s*({.*?})\s*;\s*$',
+                          main_script.string, 
+                          flags=re.DOTALL | re.MULTILINE).group(1)
+    main_data = json.loads(json_text)
+
+    # get numbers for our album
+    total_play = main_data['store']['AlbumDetailPage']['albumInfo']['mainInfo']['playCount']
+    subs = main_data['store']['AlbumDetailPage']['albumInfo']['mainInfo']['subscribeCount']
+    num_fans = main_data['store']['AlbumDetailPage']['albumInfo']['anchorInfo']['anchorFansCount']
+    tracks = main_data['store']['AlbumDetailPage']['albumInfo']['tracksInfo']['tracks']
+
+    # get publish date for each track
+    for track in tracks:
+        track_url = 'https://www.ximalaya.com'+track['url']
+        track_soup = BeautifulSoup(requests.get(track_url, headers = Headers).text)
+        track['publish_date'] = track_soup.find('span', {'class':'time _Td'}).text
+
+    # final dataframe to save
+    tracks_df = pd.DataFrame(tracks)
+    tracks_df['album_total_play'] = total_play
+    tracks_df['album_subs'] = subs
+    tracks_df['num_fans'] = num_fans
+    tracks_df['datapull_dt'] = datetime.date.today()
+
+    tracks_df.to_csv(output_path+'ximalaya_data_{}.csv'.format(datetime.date.today()),encoding='utf_8_sig')
+
+
+if __name__ == '__main__':
+	main()
